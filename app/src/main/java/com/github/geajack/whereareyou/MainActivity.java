53b72c024647815797f79ad0da7dd92e6721f48f
Jack <->
2020-07-15 16:21:51 +0200

    Display a notification when an SMS is received

--- app/src/main/java/com/github/geajack/whereareyou/MainActivity.java
+++ app/src/main/java/com/github/geajack/whereareyou/MainActivity.java
@@ -3,8 +3,14 @@ package com.github.geajack.whereareyou;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,7 +18,7 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/github/geajack/whereareyou/SMSReceiver.java
+++ app/src/main/java/com/github/geajack/whereareyou/SMSReceiver.java
@@ -1,12 +1,40 @@
 package com.github.geajack.whereareyou;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
 public class SMSReceiver extends BroadcastReceiver {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onReceive(Context context, Intent intent) {
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "Channel")
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("Where Are You?")
+                .setContentText("This is a test notification.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        CharSequence name = context.getString(R.string.channel_name);
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel("Channel", name, importance);
+
+        NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+
+        notificationManager.notify(0, builder.build());
     }
 }
