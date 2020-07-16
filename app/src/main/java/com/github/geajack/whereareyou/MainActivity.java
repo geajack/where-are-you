71d73d846c0025b052b471efb89fde8f27d093d7
Jack <->
2020-07-17 00:30:10 +0200

    Handle permissions better in MainActivity

--- app/src/main/java/com/github/geajack/whereareyou/MainActivity.java
+++ app/src/main/java/com/github/geajack/whereareyou/MainActivity.java
@@ -1,21 +1,18 @@
 package com.github.geajack.whereareyou;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
 import android.Manifest;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,18 +20,49 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED || true)
+        String[] requiredPermissions = {
+                Manifest.permission.RECEIVE_SMS,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.SEND_SMS
+        };
+
+        List<String> permissionsToRequest = new ArrayList<>();
+        for (int i = 0; i < requiredPermissions.length; i++)
         {
-            String[] permissions = {
-                    Manifest.permission.RECEIVE_SMS,
-                    Manifest.permission.ACCESS_BACKGROUND_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.SEND_SMS
-            };
-            requestPermissions(permissions, 1000);
+            if (checkSelfPermission(requiredPermissions[i]) != PackageManager.PERMISSION_GRANTED)
+            {
+                permissionsToRequest.add(requiredPermissions[i]);
+            }
+        }
+
+        if (permissionsToRequest.size() > 0)
+        {
+            setContentView(R.layout.permissions_page);
+            Button grantPermissionsButton = (Button) findViewById(R.id.grantPermissionsButton);
+            grantPermissionsButton.setOnClickListener(new PermissionsButtonListener(this, (String[]) permissionsToRequest.toArray(new String[0])));
+        }
+        else
+        {
+            setContentView(R.layout.activity_main);
+        }
+    }
+
+    static private class PermissionsButtonListener implements View.OnClickListener {
+        private Activity parentActivity;
+        private final String[] permissionsToRequest;
+
+        public PermissionsButtonListener(Activity parentActivity, String[] permissionsToRequest) {
+            this.parentActivity = parentActivity;
+            this.permissionsToRequest = permissionsToRequest;
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.M)
+        @Override
+        public void onClick(View button) {
+            parentActivity.requestPermissions(permissionsToRequest, 1000);
         }
     }
 }
